572c7e4689d15e9473ed26d6a6d26021b3f7712e
banxi1988 <banxi1988@gmail.com>
2019-02-18 16:21:14 +0800

    feat(orm) 从 BaseModel 中提取出 CreatedUpdatedMixin
    直接原因是 mptt 需要继承自对应的 Model

--- wepost/base/models.py
+++ wepost/base/models.py
@@ -5,12 +5,13 @@ from django.urls import reverse
 __author__ = 'banxi'
 
 
-class BaseModel(models.Model):
+class CreatedUpdatedMixin(models.Model):
   updated = models.DateTimeField(auto_now=True, verbose_name='最后更新')
   created = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+  class Meta:
+    abstract = True
 
-  objects = models.Manager() # 默认查询器，再次添加，方便自动补全
-
+class BaseModel(CreatedUpdatedMixin):
   class Meta:
     abstract = True
 
