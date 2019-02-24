825be2151a42ba2dc3e0578d0489a5ce35e4f92b
banxi1988 <banxi1988@gmail.com>
2019-02-24 22:52:30 +0800

    feat(test) 让一些开发账号随便关注一些节点以方便后面开发测试 SQL

--- wepost/apps/posts/tests/test_user_node_service.py
+++ wepost/apps/posts/tests/test_user_node_service.py
@@ -28,3 +28,14 @@ def test_toggle_star_node(ut1, node_c, node_py, node_js):
     service1.unblock_node(node)
     assert not service1.has_star(node)
     assert not service1.has_block(node)
+
+
+def test_query_nodes(ut1, node_c, node_py, node_js, node_flask):
+  service = UserNodeService(ut1)
+  nodes = [node_c, node_js, node_py, node_flask]
+  for node in nodes:
+    service.star_node(node)
+
+  ret_nodes1 = list(service.query_star_nodes())
+  assert len(ret_nodes1) == len(nodes)
+  assert ret_nodes1 == nodes
