ba107a3e0fa6818a4239bf404548b1a70ad4cc07
banxi1988 <banxi1988@gmail.com>
2019-03-30 19:32:24 +0800

    feat(docker) 演示版分支 （2）数据存储与挂载

--- rundev.sh
+++ rundev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t wepost:dev -f Dockerfile .
-docker run --rm --name wepost-dev -p 8001:8000 wepost:dev ./manage_dev.py runserver
+docker run --rm --name wepost-dev --mount type=volume,src=wepost-db,target=/opt/data/ -p 8001:8000 wepost:dev ./manage_dev.py runserver 0.0.0.0:8000
--- wepost/conf/base.py
+++ wepost/conf/base.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'wepost.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
     }
 }
 
